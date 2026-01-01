7ee79e60631bfdfb29ab4166f66f555f39cefe9a
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2026-01-01 14:05:52 +0900

    Add Aqua and JET dynamically in tests for Julia 1.11+, run CI only on Julia 1

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,11 @@ using Test
 using LinearAlgebra
 using Random
 
-# Skip Aqua and JET tests in CI to avoid Julia version dependency issues
-if get(ENV, "CI", "false") == "false"
+# Run Aqua and JET tests only on Julia 1.11+ to avoid version dependency issues
+if VERSION >= v"1.11"
+    using Pkg
+    Pkg.add("Aqua")
+    Pkg.add("JET")
     include("test_with_aqua.jl")
     include("test_with_jet.jl")
 end
